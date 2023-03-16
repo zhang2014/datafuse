4017f2d3adca8c97cb276c81b40b4b4e739e1a11
zhang2014 <coswde@gmail.com>
2023-03-16 19:55:47 +0800

    fix(cluster): update

--- src/query/service/src/api/rpc/flight_client.rs
+++ src/query/service/src/api/rpc/flight_client.rs
@@ -231,7 +231,6 @@ impl FlightExchange {
                         Either::Left((_, right)) => {
                             debug_assert!(state.closed_both());
 
-                            // break 'loop_worker;
                             tx.close();
                             drop(network_tx);
                             response_tx.close();
@@ -467,6 +466,12 @@ impl FlightExchange {
 
             'publisher_worker: loop {
                 if channel_state.closed_both() {
+                    while let Ok(response) = response_rx.try_recv() {
+                        if network_tx.send(response).await.is_err() {
+                            break 'publisher_worker;
+                        }
+                    }
+
                     break 'publisher_worker;
                 }
 
